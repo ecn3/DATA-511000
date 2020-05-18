51e34987f038abcdf6b9b5616184ae2bd9f25622
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-05-18 18:35:25 -0500

    1. Print header info to screen done

--- kMeans.py
+++ kMeans.py
@@ -2,3 +2,9 @@
 # 5/18/2020
 # DATA-51100-002, SUMMER 2020
 # PROGRAMMING ASSIGNMENT #2 kMeans
+
+# Print header info to screen
+print("Christian Nelson | Kanar Ibrahim")
+print("5/18/2020")
+print("DATA-51100-002, SUMMER 2020")
+print("PROGRAMMING ASSIGNMENT #2 kMeans")
